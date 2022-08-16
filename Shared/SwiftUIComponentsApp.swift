e1ecd24f4ba01d31909d1e2b2a952ec5d2df6f17
Porter McGary <56183563+mcgaryp@users.noreply.github.com>
2022-08-15 20:59:42 -0700

    Initial Commit

---
+++ Shared/SwiftUIComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComponentsApp.swift
+//  Shared
+//
+//  Created by Porter McGary on 8/15/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
